e485a5841970833b6735e0edf4438fd60f954498
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-03-18 23:58:30 +0100

    add parse to gradle build

--- app/src/main/java/com/example/uberclone/StarterApplication.kt
+++ app/src/main/java/com/example/uberclone/StarterApplication.kt
@@ -1,4 +1,36 @@
 package com.example.uberclone
 
-class StarterApplication {
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.parse.Parse
+import com.parse.ParseACL
+import com.parse.ParseException
+import com.parse.ParseObject
+import com.parse.SaveCallback
+
+class StarterApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this)
+
+        // Add your initialization code here
+        Parse.initialize(Parse.Configuration.Builder(applicationContext)
+                .applicationId("myappID")
+                .clientKey("hCxsL9CG4krg")
+                .server("http://3.70.204.147//parse/")
+                .build()
+        )
+
+        //ParseUser.enableAutomaticUser()
+        val defaultACL = ParseACL()
+        defaultACL.publicReadAccess = true
+        defaultACL.publicWriteAccess = true
+        ParseACL.setDefaultACL(defaultACL, true)
+
+    }
 }
